4dd1edb314df7e4bd6f07482c21e1d9b6fa5da99
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-09-30 11:01:15 +0100

    Login view

---
+++ lib/auth/login/login_state.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_auth/auth/form_submission_status.dart';
+
+class LoginState {
+  final String username;
+  final String password;
+  final FormSubmissionStatus formStatus;
+
+  LoginState({
+    this.username = '',
+    this.password = '',
+    this.formStatus = const InitialFormStatus(),
+  });
+
+  LoginState copyWith({
+    required String username,
+    required String password,
+    required FormSubmissionStatus formStatus,
+  }) {
+    return LoginState(
+        username: username ?? this.username,
+        password: password ?? this.password,
+        formStatus: formStatus ?? this.formStatus);
+  }
+}
